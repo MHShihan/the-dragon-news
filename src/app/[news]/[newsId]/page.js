45b8efd6e9422b28182ad08ffc8f19c7c4737f24
MHShihan <mahmodul.no.1@gmail.com>
2024-02-05 18:31:25 +0600

    Created single data fetching function

--- src/app/[news]/[newsId]/page.js
+++ src/app/[news]/[newsId]/page.js
@@ -1,5 +1,8 @@
-const NewsDetailPage = ({ params }) => {
-  console.log(params);
+import { getSingleData } from "@/utils/getSingleNews";
+
+const NewsDetailPage = async ({ params }) => {
+  const { data } = await getSingleData(params.newsId);
+  console.log(data);
   return (
     <div>
       <h1>{params.newsId}</h1>
